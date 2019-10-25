03b046d5b7605b7c68084604daba9edf26c4e8a2
Mr.X <48942851+Mr-XsZ@users.noreply.github.com>
2019-10-25 21:48:22 +0700

    Ok

---
+++ menu/friend.py
@@ -0,0 +1,41 @@
+def acc_all_friend():
+	acc = str(input('\n   [?] type "yes" to confirm: '))
+	if acc != "yes":
+		echo("[+] Operation Canceled")
+		enter()
+	gas = Friend()
+	gas.dump_sts("https://mbasic.facebook.com/friends/center/requests/", "Konfirmasi", "Lihat selengkapnya", 0, "com")
+	print()
+	echo("[+] Total: " + str(len(gas.id)))
+	echo("[+] Process")
+	gas.hajar()
+	echo("[+] Done!")
+	enter()
+
+def rej_all_friend():
+	acc = str(input('\n   [?] type "yes" to confirm: '))
+	if acc != "yes":
+		print()
+		echo("[+] Operation Canceled")
+		enter()
+	gas = Friend()
+	gas.dump_sts("https://mbasic.facebook.com/friends/center/requests/", "Hapus Permintaan", "Lihat selengkapnya", 0, "com")
+	print()
+	echo("[+] Total: " + str(len(gas.id)))
+	echo("[+] Process")
+	gas.hajar()
+	echo("[+] Done!")
+	enter()
+	
+def unadd_all_friend():
+	acc = str(input('\n   [?] type "yes" to confirm: '))
+	if acc != "yes":
+		print("\n   [+] Operation Canceled")
+		enter()
+	gas = Friend()
+	gas.dump_sts("https://mbasic.facebook.com/friends/center/requests/outgoing/", "Batalkan Permintaan", "Lihat selengkapnya", 0, "com")
+	print("\n   [+] Total: " + str(len(gas.id)))
+	print("   [+] Process")
+	gas.hajar()
+	print("   [+] Done!")
+	enter()
